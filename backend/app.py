008ad2151e77ebde4075c9f0a3c47a91e28627d2
Artem-Tkachev <tkachev.artem.a@gmail.com>
2026-08-01 02:17:20 +0100

    fixed all syntax errors in tables

--- backend/app.py
+++ backend/app.py
@@ -8,11 +8,11 @@ conn.row_factory = sqlite3.Row
 conn.execute("PRAGMA foreign_keys = ON")
 cursor = conn.cursor()
 
-cursor.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, username UNIQUE KEY TEXT, password TEXT, created_at DATETIME)") #db for users
-cursor.execute("CREATE TABLE routes (id INTEGER PRIMARY KEY, name TEXT, map_data JSON, created_by INTEGER, FOREIGN KEY (created_by) REFERENCES users(id))") #db for routes
-cursor.execute("CREATE TABLE favorites (id INTEGER PRIMARY KEY, user_id INTEGER, route_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(id), FOREIGN KEY (route_id) REFERENCES routes(id))") #db for favorites
-cursor.execute("CREATE TABLE challenges (id INTEGER PRIMARY KEY, from_user_id INTEGER, to_user_id INTEGER, route_id INTEGER, status TEXT, FOREIGN KEY (from_user_id) REFERENCES users(id), FOREIGN KEY (to_user_id) REFERENCES user_id, FOREIGN KEY (route_id) REFERENCES routes(id)") #db for challenges
-cursor.execute("CREATE TABLE workouts (id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT, type TEXT, distance FLOAT, duration INTEGER, route_id INTEGER, created_at DATETIME, FOREIGN KEY user_id REFERENCES users(id), FOREIGN KEY route_id REFERENCES route(id))") #db for workouts
+cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT, created_at DATETIME)") #db for users
+cursor.execute("CREATE TABLE IF NOT EXISTS routes (id INTEGER PRIMARY KEY, name TEXT, map_data JSON, created_by INTEGER, FOREIGN KEY (created_by) REFERENCES users(id))") #db for routes
+cursor.execute("CREATE TABLE IF NOT EXISTS favorites (id INTEGER PRIMARY KEY, user_id INTEGER, route_id INTEGER, FOREIGN KEY (user_id) REFERENCES users(id), FOREIGN KEY (route_id) REFERENCES routes(id))") #db for favorites
+cursor.execute("CREATE TABLE IF NOT EXISTS challenges (id INTEGER PRIMARY KEY, from_user_id INTEGER, to_user_id INTEGER, route_id INTEGER, status TEXT, FOREIGN KEY (from_user_id) REFERENCES users(id), FOREIGN KEY (to_user_id) REFERENCES users(id), FOREIGN KEY (route_id) REFERENCES routes(id))") #db for challenges
+cursor.execute("CREATE TABLE IF NOT EXISTS workouts (id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT, type TEXT, distance FLOAT, duration INTEGER, route_id INTEGER, created_at DATETIME, FOREIGN KEY (user_id) REFERENCES users(id), FOREIGN KEY (route_id) REFERENCES route(id))") #db for workouts
 
 
 conn.commit()
