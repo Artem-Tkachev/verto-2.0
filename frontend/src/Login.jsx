d657dd8d80388ccc9500dd7de262e03a746a21d1
Artem-Tkachev <tkachev.artem.a@gmail.com>
2026-08-02 01:43:41 +0100

    syncronise back and front in login. user now can login

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -3,17 +3,39 @@ import { useState } from "react";
 function Login(){
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   
+  async function loginSubmit(e){
+    e.preventDefault();
+    
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify({username: username, password: password})
+    });
+
+    const data = await response.json();
+    console.log(data);
+    if(response.ok){
+      setMessage(data.message);
+    }
+    else{
+      setMessage(data.error);
+    }
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={loginSubmit}>
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
         <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button type="submit">login</button>
       </form>
-      
+      <p>{message}</p>
     </div>
   )
 }
+
+
 
 export default Login
