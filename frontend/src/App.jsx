9a1e2db9c2299b5c2c4720ba9fdd4d3b7388f897
Artem-Tkachev <tkachev.artem.a@gmail.com>
2026-08-26 13:05:20 +0100

    changed login to Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "./login";
+import Login from "./Login";
 import Registration from "./registration";
 import Dashboard from "./dashboard";
 import Profile from "./profile";
